889dec1a2e5e0be2ee531d9de4e7ab7268ea87de
Felipe Carrasco P <carrascopatelli.fa@gmail.com>
2024-03-26 22:02:41 -0300

    Versión 1.0 - Desafio 1

---
+++ ecmaScript/app.js
@@ -0,0 +1,41 @@
+class ProductManager {
+    constructor() {
+        this.products = []
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+        const verificarCode = this.products.some((item)=> item.code == code);
+        if (verificarCode) {
+            console.log("Código ya existente")
+        }
+        else {
+            const id_producto = this.products.length + 1;
+            let producto = {
+                id : id_producto,
+                tituto: title,
+                descripcion: description,
+                precio: price,
+                imagen: thumbnail,
+                codigo: code,
+                disponibilidad: stock
+            }
+            this.products.push(producto)
+        }
+    }
+
+    getProduct() {
+        console.log(this.products)
+    }
+
+    getProductById(id_item) {
+        let  item = (this.products.find((item) => item.id === id_item)) ? console.log((this.products.find((item) => item.id === id_item))) : console.log("Not found")
+        
+    }
+}
+
+const Manejador = new ProductManager;
+
+
+Manejador.addProduct("Item sexo", "Descripción sexo", 12300, "link.cl", 13422, 3);
+Manejador.getProduct();
+Manejador.getProductById(2);
